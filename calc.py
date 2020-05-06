413aaae0c79a3c64ecb7ef0c6eab9cd173bdd234
Root user from new-ansible-target-3 <root@new-ansible-target-3>
2020-05-06 05:24:11 -0400

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,21 @@
+def add_value(x,y):
+  return x + y
+
+def substract_value(x,y):
+  pass
+
+def multiply_value(x,y):
+  pass
+
+def divide_value (x,y):
+  pass
+
+def square_value (x):
+  pass
+
+
+x = int(input('Enter Value for X : ' ))
+y = int(input('Enter Value for Y : ' ))
+print(x,y)
+
+print("Addition of Values {}".format(add_value(x,y)))
