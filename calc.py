fba958d7a42b823df0c5c4032ebc6262611e0be5
Root user from new-ansible-target-3 <root@new-ansible-target-3>
2020-05-06 13:51:38 -0400

    Updated Square Functiond

--- calc.py
+++ calc.py
@@ -11,13 +11,14 @@ def divide_value (x,y):
   pass
 
 def square_value (x):
-  pass
+  return x ** 2
 
 
 x = int(input('Enter Value for X : ' ))
 y = int(input('Enter Value for Y : ' ))
 print(x,y)
 
-print("Addition Value {}".format(add_value(x,y)))
-print("Subtraction  Value {}".format(substract_value(x,y)))
-print("Multiiplication Value {}".format(multiply_value(x,y)))
+print("Addition Value           : {} ".format(add_value(x,y)))
+print("Subtraction  Value       : {} ".format(substract_value(x,y)))
+print("Multiiplication Value    : {} ".format(multiply_value(x,y)))
+print("Square Value  of {}      : {} ".format(x, multiply_value(x)))
